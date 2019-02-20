1aa1581ecd452b3e5f4e86d882c09f91f6c18db2
hendisantika <hendisantika@yahoo.co.id>
2019-02-21 05:35:39 +0700

    #13 - Still error when hit request - Not Authorized occures

--- django_rest_api_jwt/urls.py
+++ django_rest_api_jwt/urls.py
@@ -18,7 +18,11 @@ from rest_framework.authtoken.views import obtain_auth_token  # <-- Here
 
 from myapi import views
 
+# from simplejwt import views as jwt_views
+
 urlpatterns = [
     path('hello/', views.HelloView.as_view(), name='hello'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # <-- And here
+    # path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
