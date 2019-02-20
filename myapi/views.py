a7c741d1e41c7089986c9c9ac48803165c0c5ebe
hendisantika <hendisantika@yahoo.co.id>
2019-02-20 07:08:46 +0700

    #7 - Add Security in Hello api in views.py file

--- myapi/views.py
+++ myapi/views.py
@@ -1,9 +1,12 @@
 # Create your views here.
+from rest_framework.permissions import IsAuthenticated  # <-- Here
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request):
         content = {'message': 'Hello, World!'}
         return Response(content)
