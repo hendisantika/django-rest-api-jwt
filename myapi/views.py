cfbb0036db84c0444b3fff716039bd8d05ca3d1c
hendisantika <hendisantika@yahoo.co.id>
2019-02-20 07:05:40 +0700

    #5 - Add HelloView in views.py file

--- myapi/views.py
+++ myapi/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class HelloView(APIView):
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
